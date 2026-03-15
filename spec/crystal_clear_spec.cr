aa89f6ee4e2722bdc65bfe8e7b7259fcc5950dae
Lasse Brandt Thomsen <lasse@bitmand.dk>
2026-03-15 12:32:01 +0100

    some very crude tests

--- spec/crystal_clear_spec.cr
+++ spec/crystal_clear_spec.cr
@@ -1,9 +1,45 @@
 require "./spec_helper"
 
 describe CrystalClear do
-  # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  # ensure shard version and CrystalClear version is the same
+  it "checks shard vs lib version" do
+    shard_version = ""
+    if File.read("shard.yml") =~ /version: ([0-9\.]+)/
+      shard_version = $1
+    end
+    shard_version.should eq(CrystalClear::VERSION)
   end
+
+  # runs examples and check raw output
+  it "checks colors example output" do
+    Process.run("crystal", args: ["run", "example/colors.cr"], output: stdout = IO::Memory.new)
+    stdout.to_s.should eq(File.read("spec/colors.raw"))
+  end
+
+  # hyperlink
+  it "checks hyperlink" do
+    link = CrystalClear::Hyperlink.new("https://github.com/bitmand/crystal_clear", "CrystalClear Github page")
+    link.print.should eq(File.read("spec/hyperlink.raw"))
+  end
+
+  # spinner
+  it "checks spinner example output" do
+    Process.run("crystal", args: ["run", "example/spinner.cr"], output: stdout = IO::Memory.new)
+    stdout.to_s.should eq(File.read("spec/spinner.raw"))
+  end
+
+  # terminal size
+  it "checks terminal size" do
+    term = CrystalClear::Terminal.new
+    term.size.cols.should be > 0
+    term.size.cols.should be < 1000
+    term.size.rows.should be > 0
+    term.size.rows.should be < 1000
+    term.size.width.should be > 0
+    term.size.width.should be < 10000
+    term.size.height.should be > 0
+    term.size.height.should be < 10000
+  end
+
 end
